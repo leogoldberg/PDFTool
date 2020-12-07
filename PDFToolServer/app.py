1c066a7990896f00a31157408ecb3e4ec1c6aeaa
lgoldber <lgoldber@sfu.ca>
2020-12-06 16:06:52 -0800

    Python rest api for communicatin with web app and java tool

---
+++ PDFToolServer/app.py
@@ -0,0 +1,51 @@
+from flask import Flask, request, render_template, session, flash, redirect, \
+    url_for, jsonify
+from celery import Celery
+
+app = Flask(__name__)
+
+
+@celery.task(bind=True)
+def img_2_pdf(self):
+    return 1
+
+
+@app.route('/img2pdf', methods=['POST'])
+def img2pdf():
+    task = img_2_pdf.apply_async()
+    return jsonify({}), 202, {'Location': url_for('taskstatus',
+                                                  task_id=task.id)}
+
+
+@app.route('/status/<task_id>')
+def taskstatus(task_id):
+    task = long_task.AsyncResult(task_id)
+    if task.state == 'PENDING':
+        response = {
+            'state': task.state,
+            'current': 0,
+            'total': 1,
+            'status': 'Pending...'
+        }
+    elif task.state != 'FAILURE':
+        response = {
+            'state': task.state,
+            'current': task.info.get('current', 0),
+            'total': task.info.get('total', 1),
+            'status': task.info.get('status', '')
+        }
+        if 'result' in task.info:
+            response['result'] = task.info['result']
+    else:
+        # something went wrong in the background job
+        response = {
+            'state': task.state,
+            'current': 1,
+            'total': 1,
+            'status': str(task.info),  # this is the exception raised
+        }
+    return jsonify(response)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
